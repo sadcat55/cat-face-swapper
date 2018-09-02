09bf95cc68d5480c05fd9a4c267400b29e8798ba
sadcat55 <42909999+sadcat55@users.noreply.github.com>
2018-09-02 15:25:18 +0100

    pastes sadcat into picture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../pycatfd')
 import catfd
+from PIL import Image
 
 def main():
 	print("Hello world!")
@@ -12,7 +13,14 @@ def main():
 	face_color = [0,0,0]
 	landmark_color = [0,0,0]
 	save_chip = True
-	catfd.detect(input_image, output_path, use_json, annotate_faces,
-	   annotate_landmarks, face_color, landmark_color, save_chip)
+
+	#list_of_cat_faces = catfd.detect(input_image, output_path, use_json, annotate_faces,
+	#   annotate_landmarks, face_color, landmark_color, save_chip)
+
+	sadcat_image = Image.open("./images/sadcat-transparent.png").convert("RGBA")
+	target_image = Image.open(input_image).convert("RGBA")
+	target_image.alpha_composite(sadcat_image)
+	target_image.save("output.png")
+
 
 main()
