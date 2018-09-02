4cbc9e229d5615f59b08c5a1c1319663902a1277
sadcat55 <42909999+sadcat55@users.noreply.github.com>
2018-09-02 14:34:21 +0100

    try to detect sample image

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import catfd
 
 def main():
 	print("Hello world!")
-	catfd.detect()
-
+	input_image = "./images/samples/nero.jpg"
+	output_path = "."
+	use_json = True
+	annotate_faces = False
+	annotate_landmarks = False
+	face_color = [0,0,0]
+	landmark_color = [0,0,0]
+	save_chip = True
+	catfd.detect(input_image, output_path, use_json, annotate_faces,
+	   annotate_landmarks, face_color, landmark_color, save_chip)
 
 main()
